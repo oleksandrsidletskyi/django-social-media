d13023bd3953d6ac2f90eeaa2fb3312420c93196
Oleksandr Sidletskyi <101951666+oleksandrsidletskyi@users.noreply.github.com>
2022-06-28 12:50:54 +0100

    Profiles views, templates, etc.

--- pythonproject/socialbook/socialbook/settings.py
+++ pythonproject/socialbook/socialbook/settings.py
@@ -106,12 +106,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
-    BASE_DIR, 'static_project'
+    os.path.join(BASE_DIR, 'static_project')
 ]
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
