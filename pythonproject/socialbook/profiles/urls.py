af1272f1ef6a0d4463006bdc3ca178b20dc6074d
Oleksandr Sidletskyi <101951666+oleksandrsidletskyi@users.noreply.github.com>
2022-07-09 17:55:13 +0100

    Worked on relationship functionality

--- pythonproject/socialbook/profiles/urls.py
+++ pythonproject/socialbook/profiles/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import my_profile_view, invites_received_view, profiles_list_view, invite_profiles_list_view, ProfileListView
+from .views import my_profile_view, invites_received_view, profiles_list_view, invite_profiles_list_view, ProfileListView, send_invatation, remove_from_friends
 
 app_name = 'profiles'
 
@@ -8,4 +8,6 @@ urlpatterns = [
     path('my-invites/', invites_received_view, name='my-invites-view'),
     path('all-profiles/', ProfileListView.as_view(), name='all-profiles-view'),
     path('to-invite/', invite_profiles_list_view, name='invite-profiles-view'),
+    path('send-invite/', send_invatation, name='send-invite'),
+    path('remove-friend/', remove_from_friends, name='remove-friend'),
 ]
